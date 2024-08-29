61f1dc47e896b3ea532fba7e9589fac7db176b39
Benjamin <benjaminbelloeil03@gmail.com>
2024-08-28 20:39:45 -0600

    Initial Commit

---
+++ StudentCalendar/StudentCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentCalendarApp.swift
+//  StudentCalendar
+//
+//  Created by Benjamin Belloeil on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct StudentCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
